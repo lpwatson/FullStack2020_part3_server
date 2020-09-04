dc458d5b67250de49b5e6d0dfc7503821279b640
Liam Watson <lwatson@shopkeep.com>
2020-09-04 10:41:58 +0100

    exercises 3.5 and 3.6

--- phonebook_server/index.js
+++ phonebook_server/index.js
@@ -32,6 +32,13 @@ let persons = [
   }
 ]
 
+const generateId = () => {
+  const maxID = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxID + 1
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 });
@@ -40,7 +47,7 @@ app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
 
-  if (person) {
+  if (person.name) {
     response.json(person)
   } else {
     response.status(404).end()
@@ -52,6 +59,33 @@ app.get('/info', (request, response) => {
     `<p>Phonebook has info for ${persons.length} people<p>
      <p>${new Date()}<p>`
     );
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  if(persons.find(p => p.name === person.name)) {
+    return response.status(409).json({
+      error: 'name must be unique'
+    })
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
